f8abab8b1a2c065d84977fbd08210c804355d811
Luca Adam <luca.adam23@gmail.com>
2023-11-15 07:16:44 +0300

    Ajout de fitre

--- frontend/src/Clients.js
+++ frontend/src/Clients.js
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import DataTable from 'react-data-table-component';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function ClientList() {
   const [clients, setClients] = useState([]);
+  const [dataFil, setFilterData] = useState([]);
 
   useEffect(() => {
     // Effectuez une requête GET vers votre API pour récupérer les données de la table excel_data
@@ -17,7 +21,7 @@ function ClientList() {
   }, []);
 
   const clientList = clients.map(client => new Client(
-    client.RefClient, client.RefClient, client.nom,
+    client.RefClient, client.RefCredit, client.nom,
     client.MontantAbandonnee, client.DatePassagePerte,
     client.CAResponsable, client.Agence, client.Type
   ));
@@ -40,10 +44,50 @@ function ClientList() {
     selectAllRowsItemText: 'Tous',
   };
 
+  function handleFiilter(event) {
+    const dataFilter = clientList.filter(row => {
+      return row.refClient.toLowerCase().includes(event.target.value.toLowerCase()) || row.refCredit.toLowerCase().includes(event.target.value.toLowerCase());
+    });
+    setFilterData(dataFilter);
+  }
+
   return (
     <div>
+      <h1>Liste des Clients</h1>
+      <Row className='mb-3 px-3'>
+        <Col>
+          <Form.Label htmlFor="ref-client">Referecnce client</Form.Label>
+          <Form.Select id='ref-client' aria-label="Referecnce client" size='sm' onChange={handleFiilter}>
+            <option>RefClient</option>
+            {clientList.map((opt) => (
+              <option key={opt.refClient} value={opt.refClient}>
+                {opt.refClient}
+              </option>
+            ))}
+          </Form.Select>
+        </Col>
+        <Col>
+          <Form.Label htmlFor="ref-credit">Referecnce credit</Form.Label>
+          <Form.Select id='ref-client' aria-label="Referecnce credit" size='sm' onChange={handleFiilter}>
+            <option>RefCredit</option>
+            {clientList.map((opt) => (
+              <option key={opt.refCredit} value={opt.refCredit}>
+                {opt.refCredit}
+              </option>
+            ))}
+          </Form.Select>
+        </Col>
+        <Col>
+          <Form.Label htmlFor="autre-critere">Password</Form.Label>
+          <Form.Control
+            type="text"
+            id="autre-critere"
+            aria-describedby="autre critere de recherche"
+            size='sm'
+          />
+        </Col>
+      </Row>
       <DataTable
-        title="Liste des Clients"
         columns={columns}
         data={clientList}
         dense
@@ -51,14 +95,11 @@ function ClientList() {
         pagination
         paginationComponentOptions={paginationComponentOptions}
         fixedHeader
-        fixedHeaderScrollHeight="550px"
+        fixedHeaderScrollHeight="450px"
         highlightOnHover
         pointerOnHover
         persistTableHead
         responsive
-        subHeader
-        subHeaderAlign="right"
-        subHeaderWrap
       />
     </div>
   );
